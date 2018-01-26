ebfaa65b72d54375c057003f263e90522291503b
Brendan <brendanarchangelo@gmail.com>
2018-01-26 03:00:27 +0000

    added facebook login

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form className="facebookLogin">
+          <br></br>
+          <label for="username">Username</label>
+          <input type="text" name="username" id="username"></input>
+          <br></br>
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password"></input>
+          <br></br>
+          <button type="submit" value="Login">Login</button>
+        </form>
       </div>
     );
   }
